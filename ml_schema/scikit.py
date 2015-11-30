da63f6257ff627a8bf61556e8d1f2ace6ae051c4
Kyle Ellrott <kellrott@gmail.com>
2015-11-30 13:36:19 -0800

    Adding python test code to read in model produced in R

--- ml_schema/scikit.py
+++ ml_schema/scikit.py
@@ -1,6 +1,8 @@
 
-
+import numpy as np
 from ml_schema import ml_schema_pb2
+from sklearn.linear_model.base import LinearModel
+from sklearn.linear_model import LinearRegression
 
 def proto_randomforest(clf):
     forest = ml_schema_pb2.RandomForestData()
@@ -37,4 +39,41 @@ def proto_linear(clf, feature_names=None):
         ]
     )
     return model
+
+
+class MLModel(LinearModel):
+    def __init__(self, msg, feature_names=None):
+        self.models = []
+        self.coef = []
+        for m in msg.Structure.Components:
+            s = None
+            if m.LinearCoeff:
+                s = LinearRegression()
+                s.intercept_ = m.LinearCoeff.Intercept
+                if feature_names is None:
+                    s.coef_ = np.zeros(len(m.LinearCoeff.Coeff))
+                else:
+                    s.coef_ = np.zeros(len(feature_names))                    
+                for i, elem in enumerate(m.LinearCoeff.Coeff):
+                    if feature_names is None:
+                        s.coef_[i] = elem.Coeff
+                    else:
+                        l = feature_names.get_loc(elem.Feature)
+                        s.coef_[l] = elem.Coeff
+            self.models.append(s)
+            if m.Coeff:
+                self.coef.append(m.Coeff)
+            else:
+                self.coef.append(1.0)
     
+    def fit(self, X, y):
+        raise Exception("ReadOnly Model")
+    
+    def decision_function(self, X):
+        out = 0.0
+        for i, mod in enumerate(self.models):
+            out += self.coef[i] * self.models[0].decision_function(X)
+        return out
+        
+def from_message(msg, feature_names=None):
+    return MLModel(msg, feature_names)
